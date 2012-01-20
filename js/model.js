b42d415bb9d21482a0ed389b8c70d3dd55e1e454
Richard Harrington <harrington.richard@gmail.com>
2012-01-19 23:14:50 -0500

    Extracted the config info back out into its own file.

--- js/model.js
+++ js/model.js
@@ -1,4 +1,4 @@
-// Dependency: util.js
+// Dependency: util.js, config.js
 
 ;
 
@@ -7,57 +7,8 @@ APP.model = (typeof APP.model !== 'undefined') ? APP.model :
 
 (function() {
     
-    // This instances array will have other properties added dynamically to its element
-    // objects. We start with "config." Later we'll add "currentBrush,"
-    // "currentPalette," and "palettes."
-    
-    var instances = [
-        {
-            config: {
-                DEFAULT_PALETTE_COLORS: ['B04141', '85224A', 'EBE3B2', '1A4F6B', '042B4F'],
-                MAX_COLORS: 10,
-                DEFAULT_PALETTE_TITLE: "default palette #1",
-                DEFAULT_COLOR_PANEL_INDEX: 0,
-                LARGE_BRUSH_WIDTH: 25,
-                SMALL_BRUSH_WIDTH: 10,
-                DEFAULT_BRUSH_SIZE: "large",
-                CANVAS_WIDTH: 600,
-                CANVAS_HEIGHT: 400,
-                CANVAS_BACKGROUND_COLOR: "EEE"
-            }
-        }, 
-        {
-            config: {
-                DEFAULT_PALETTE_COLORS: ['000000', '333333', '666666', '999999', 'BBBBBB'],
-                MAX_COLORS: 10,
-                DEFAULT_PALETTE_TITLE: "default palette #2",
-                DEFAULT_COLOR_PANEL_INDEX: 0,
-                LARGE_BRUSH_WIDTH: 50,
-                SMALL_BRUSH_WIDTH: 30,
-                DEFAULT_BRUSH_SIZE: "large",
-                CANVAS_WIDTH: 800,
-                CANVAS_HEIGHT: 300,
-                CANVAS_BACKGROUND_COLOR: "FFF"
-            }
-        }, 
-        { 
-            config: {
-                DEFAULT_PALETTE_COLORS: ['003366', '336699', '6699BB', '99BBEE', 'BBEE11',
-                                         '060E18', '002E63', '0076FE', 'DBDADC', 'D0FDFE'],
-                MAX_COLORS: 10,
-                DEFAULT_PALETTE_TITLE: "default palette #3",
-                DEFAULT_COLOR_PANEL_INDEX: 3,
-                LARGE_BRUSH_WIDTH: 80,
-                SMALL_BRUSH_WIDTH: 5,
-                DEFAULT_BRUSH_SIZE: "small",
-                CANVAS_WIDTH: 700,
-                CANVAS_HEIGHT: 1000,
-                CANVAS_BACKGROUND_COLOR: "FEB"
-            }
-        }
-    ];
-
     var util = APP.util;
+    var config = APP.config;
 
     var BrushStyle,
         brushChildrenProto;
@@ -67,8 +18,25 @@ APP.model = (typeof APP.model !== 'undefined') ? APP.model :
         CurrentBrush;
         
     var init;
-    
     var instanceNumber = 0;
+
+    // The 'instances' array will have other properties added dynamically to its element
+    // objects. We start with "config," which contains all the information from the config.js
+    // file. Later we'll add "currentBrush," "currentPalette," and "palettes."
+    
+    var instances = [];
+    var i, k, len;
+    
+    for (i = 0, len = config.length; i < len; i += 1) {
+        instances[i] = {};
+        instances[i].config = {};
+        for (k in config[i]) {
+            if (config[i].hasOwnProperty( k )) {
+                instances[i].config[k] = config[i][k]
+            }
+        }
+    }
+
 
     // --- A note about Palettes, CurrentPalette and CurrentBrush:
 
