a9446351aa5b47f560db650f835da3940799a700
Richard Harrington <harrington.richard@gmail.com>
2012-04-05 14:29:41 -0400

    io.sockets.emit not quite working yet

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , parse = require('url').parse
-  , join = require('path').join
-  , counter = 0;
+  , join = require('path').join;
 
 io.sockets.on('connection', function (socket) {
   socket.on('move', function (data) {
-    console.log("We've received move number " + counter++);
-    socket.emit('stroke', data);
+    io.sockets.emit('stroke', data);
   });
 });
 
-app.listen(3000, '10.0.1.2');
+app.listen(80, '10.0.1.2');
 
 function handler (req, res) {
   var url = parse(req.url);
-  var localPathname = (url.pathname === '/') ? 'index.html' : url.pathname;
+  var localPathname = (url.pathname === '/') ? '/index.html' : url.pathname;
   var path = __dirname + localPathname;
   
   fs.stat(path, function(err, stat) {
