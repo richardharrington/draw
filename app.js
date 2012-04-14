a1f8a4a3d49566a965d51898ea25b253af9ceafa
Richard Harrington <harrington.richard@gmail.com>
2012-04-13 23:22:18 -0400

    added configuration details for production environment -- don't think they made it any faster

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var app = require('http').createServer(handler).listen(3000)
   , brushStyleIdGen = 0
   , clearConfirmPending = false;
   
-/*
+
 
 io.configure('production', function(){
   io.enable('browser client minification');  // send minified client 
   io.enable('browser client etag');          // apply etag caching logic based on version number
   io.enable('browser client gzip');          // gzip the file
   io.set('log level', 1);                    // reduce logging
-  io.set('transports', [                     // enable all transports (optional ifyou want flashsocket)
+  io.set('transports', [                     // enable all transports (optional if you want flashsocket)
       'websocket'
-    , 'flashsocket'
+//    , 'flashsocket'
     , 'htmlfile'
     , 'xhr-polling'
     , 'jsonp-polling'
@@ -28,7 +28,7 @@ io.configure('development', function(){
   io.set('transports', ['websocket']);
 });
 
-*/
+
 
 io.sockets.on('connection', function(socket) {
     
